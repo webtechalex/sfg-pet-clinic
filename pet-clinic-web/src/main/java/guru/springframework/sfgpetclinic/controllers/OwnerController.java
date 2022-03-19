7cfd2ba50922d0c8356b5d8805dc92e849724b57
alexanderbrown <alexanderbrown.info@gmail.com>
2022-03-19 20:36:38 +0000

    fix broken links

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -20,4 +20,9 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
+    }
 }
